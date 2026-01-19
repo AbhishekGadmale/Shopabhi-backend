1c5b85593e1ff4e5a2fd55c8a844474f7d9f800f
Abhishek Gadmale <agadmale335@gmail.com>
2026-01-19 15:59:45 +0530

    ignore env file and update server

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors({
-  origin:"http://localhost:3000",
+  origin:"https://abhishek-portfolio-2.onrender.com/",
   credentials:true,
 }));
 
