b79101b17c9f0854b5fb6e428643a92ba58a24e0
Abhishek Gadmale <agadmale335@gmail.com>
2026-05-05 13:33:50 +0530

    fixed order and server mistmatch issue

--- routes/orders.js
+++ routes/orders.js
@@ -7,6 +7,7 @@ const router = express.Router();
 router.use(authMiddleware);
 
 router.get("/", orderController.getOrders);
+router.post("/", orderController.createOrder);
 router.post("/preview", orderController.previewOrder);
 router.post("/validate-coupon", orderController.validateCoupon);
 router.post("/razorpay", orderController.createRazorpayOrder);
