f95a2fa3b81bcb4c7b0905d1902fa98e8a757353
Abhishek Gadmale <agadmale335@gmail.com>
2026-05-06 22:23:49 +0530

    changes

--- models/User.js
+++ models/User.js
@@ -24,6 +24,7 @@ const userSchema=new mongoose.Schema({
             state: { type: String, required: true },
             zipCode: { type: String, required: true },
             country: { type: String, required: true },
+            phone: { type: String },
             isDefault: { type: Boolean, default: false }
         }
     ],
